35502d0a00ffd9ca78dd84815ac04769ca29d333
Poatty <148661665+Poatty@users.noreply.github.com>
2025-11-21 16:11:19 -0800

    Create Driver.java

---
+++ src/Driver.java
@@ -0,0 +1,22 @@
+public class Driver {
+    FoodEach cookie = new FoodEach("cookie", 0, 0, false);
+    FoodEach sushi = new FoodEach("sushi", 0, 0, false);
+    FoodEach boarHead = new FoodEach("boar head", 0, 0, false);
+    FoodEach apple = new FoodEach("apple", 0, 0, false);
+    FoodEach friedEgg = new FoodEach("fried egg", 0, 0, false);
+    FoodEach pineapple = new FoodEach("pineapple", 0, 0, false);
+    FoodEach fish = new FoodEach("fish", 0, 0, false);
+    FoodEach cheese = new FoodEach("cheese", 0, 0, false);
+    FoodEach chicken = new FoodEach("chicken", 0, 0, false);
+    FoodEach bread = new FoodEach("bread", 0, 0, false);
+    FoodEach pumpkinPie = new FoodEach("pumpkin pie", 0, 0, false);
+    FoodEach cheesecake = new FoodEach("cheesecake", 0, 0, false);
+    FoodEach applePie = new FoodEach("apple pie", 0, 0, false);
+    FoodEach watermelon = new FoodEach("watermelon", 0, 0, false);
+    FoodEach avocado = new FoodEach("avocado", 0, 0, false);
+    FoodEach pretzel = new FoodEach("pretzel", 0, 0, false);
+    FoodEach shrimp = new FoodEach("shrimp", 0, 0, false);
+    FoodEach tboneSteak = new FoodEach("tbone steak", 0, 0, false);
+    FoodEach honey = new FoodEach("honey", 0, 0, false);
+    FoodEach dragonfruit = new FoodEach("dragonfruit", 0, 0, false);
+}
