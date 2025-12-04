107e2ffe3eaab3c878cc9fa70d0c50e3e781431c
thisisveryweird <murzelloshaun@gmail.com>
2025-12-04 11:50:25 -0800

    Delete src/run_game.sh

--- src/run_game.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "/mnt/chromeos/MyFiles/Downloads/COMPSCI-PROJECT-main (3)/COMPSCI-PROJECT-main" || exit
-javac -d bin src/*.java 2>&1
-if [ $? -eq 0 ]; then
-    java -cp bin Main 2>&1 | head -50
-else
-    echo "Compilation failed."
-fi
-read -p "Press Enter to close..."
