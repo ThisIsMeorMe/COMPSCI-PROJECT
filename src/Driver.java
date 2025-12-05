0e566d58afa831f0d2cf7ea2613f4411432152b8
thisisveryweird <murzelloshaun@gmail.com>
2025-12-04 20:44:06 -0800

    Refactor FoodEach initialization in Driver constructor

--- src/Driver.java
+++ src/Driver.java
@@ -1,22 +1,25 @@
 public class Driver {
-    FoodEach apple = new FoodEach("apple", 3, 0, true);
-    FoodEach bread = new FoodEach("bread", 5, 10, false);
-    FoodEach cheese = new FoodEach("cheese", 10, 25, false);
-    FoodEach watermelon = new FoodEach("watermelon", 15, 50, false);
-    FoodEach fish = new FoodEach("fish", 25, 70, false);
-    FoodEach honey = new FoodEach("honey", 40, 120, false);
-    FoodEach chicken = new FoodEach("chicken", 60, 200, false);
-    FoodEach avocado = new FoodEach("avocado", 100, 350, false);
-    FoodEach shrimp = new FoodEach("shrimp", 150, 500, false);
-    FoodEach friedEgg = new FoodEach("fried egg", 200, 750, false);
-    FoodEach cookie = new FoodEach("cookie", 300, 1000, false);
-    FoodEach pineapple = new FoodEach("pineapple", 500, 1500, false);
-    FoodEach sushi = new FoodEach("sushi", 750, 2000, false);
-    FoodEach pretzel = new FoodEach("pretzel", 1000, 3000, false);
-    FoodEach tboneSteak = new FoodEach("tbone steak", 1500, 5000, false);
-    FoodEach pumpkinPie = new FoodEach("pumpkin pie", 2500, 10000, false);
-    FoodEach cheesecake = new FoodEach("cheesecake", 5000, 20000, false);
-    FoodEach applePie = new FoodEach("apple pie", 7500, 30000, false);
-    FoodEach dragonfruit = new FoodEach("dragonfruit", 10000, 50000, false);    
-    FoodEach boarHead = new FoodEach("boar head", 50000, 100000, false);
+    
+    public Driver() {
+        new FoodEach("apple", 3, 0, true);
+        new FoodEach("apple_pie", 5, 10, false);
+        new FoodEach("avocado", 10, 25, false);
+        new FoodEach("boar_head", 15, 50, false);
+        new FoodEach("bread", 25, 70, false);
+        new FoodEach("cheese", 40, 120, false);
+        new FoodEach("cheesecake", 60, 200, false);
+        new FoodEach("chicken", 100, 350, false);
+        new FoodEach("cookie", 150, 500, false);
+        new FoodEach("dragon_fruit", 200, 750, false);
+        new FoodEach("fish", 300, 1000, false);
+        new FoodEach("fried_eggs", 500, 1500, false);
+        new FoodEach("honey", 750, 2000, false);
+        new FoodEach("pineapple", 1000, 3000, false);
+        new FoodEach("pretzel", 1500, 5000, false);
+        new FoodEach("pumpkin_pie", 2500, 10000, false);
+        new FoodEach("shrimp", 5000, 20000, false);
+        new FoodEach("sushi", 7500, 30000, false);
+        new FoodEach("t-bone", 10000, 50000, false);
+        new FoodEach("watermelon", 50000, 100000, false);
+    }
 }
